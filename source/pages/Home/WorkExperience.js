6b11500c22f59eeb6e265ed2078d087ffc429a13
Billgo <cocobill@vip.qq.com>
2019-02-02 13:34:11 +0800

    update

--- source/pages/Home/WorkExperience.js
+++ source/pages/Home/WorkExperience.js
@@ -31,7 +31,7 @@ const WorkExperience = props => (
         company="LWH HOLDINGS PTE LTD" 
         location="Singapore"
         period="2015 - 2019" 
-        position="FRONTEND DEVELOPER">
+        position="FULLSTACK DEVELOPER">
         <ul className="a-article-ul">
           <li>Wrote user requirements documents by interviewed users, created product prototypes.</li>
           <li>Developed the corporate website and additional softwares for internal use, including OA, CMS and ERP systems.</li>
